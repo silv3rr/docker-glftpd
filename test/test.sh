8311958622d7c05fbb5fecdbde235379cd704a1c
silver <silv3rr@users.noreply.github.com>
2023-07-21 21:19:59 +0200

    test: add test.sh (3x login), IP in login.sh

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-CMD="debug 9; ls"
+#CMD="debug 9; ls"
+CMD="repeat 120 quote 'NOOP'"
 #IP="172.17.0.2"
 PORT=1337
+
+if [ -n "$1" ]; then
+  pkill -f 'lftp -u'
+  exit
+fi
 
 if [ -z "$IP" ]; then
   IP="$(
@@ -13,9 +19,11 @@ fi
 
 if command -v lftp >/dev/null 2>&1; then
   if [[ "$IP" =~ ^[0-9\.]{7,} ]] && [[ "$PORT" =~ ^[0-9]{1,5}$ ]]; then
-    echo "TEST CMD: '$CMD' (DOCKER IP: $IP PORT: $PORT) ... "
-    echo
-    lftp -e "set ssl:verify-certificat no; $CMD" -u glftpd,glftpd "${IP}:${PORT}"
+    for i in $(seq 0 2); do
+      echo "$i: TEST CMD: '$CMD' (DOCKER IP: $IP PORT: $PORT) ... "
+      echo
+      lftp -u glftpd,glftpd "${IP}:${PORT}" -e "set ssl:verify-certificat no; $CMD" &
+    done
   else
     echo "ERROR: ip/port not found, exiting..."
     exit 1
