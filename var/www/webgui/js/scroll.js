978e2b057d82dc8db45a85b9ee3b9a1ed55291fb
silver <silv3rr@users.noreply.github.com>
2024-02-03 13:36:06 +0100

    webgui: move to files

--- var/www/webgui/js/scroll.js
+++
@@ -1,9 +0,0 @@
-function gotoTop() {
-    document.body.scrollTop=0;
-    document.documentElement.scrollTop=0;
-};
-
-function gotoEnd() {
-    var scrollingElement = (document.scrollingElement || document.body);
-    scrollingElement.scrollTop = scrollingElement.scrollHeight;
-};
